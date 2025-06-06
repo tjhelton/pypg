79fcdf64fa440e7857eabd74db7d6e758720d664
tjhelton99 <140657018+tjhelton99@users.noreply.github.com>
2025-06-05 22:32:42 -0500

    mass update

---
+++ scripts/sc/create_sites/main.py
@@ -0,0 +1,61 @@
+import requests
+import pandas as pd
+import os
+
+TOKEN = '14a6425122c93f475f5f242188b5692e6f40641117797154c3a1f7899b6d6346'
+
+def read_csv():
+    csv_df = pd.read_csv('input.csv')
+    csv_df = csv_df.fillna('')
+    csv = csv_df.to_dict('records')
+    return csv
+
+def create_site(name, parent, count):
+    try:
+        url = "https://api.safetyculture.io/directory/v1/folder"
+        if parent == '':
+            payload = {
+                "meta_label": "location",
+                "name": name
+            }
+        else:
+            payload = {
+                "meta_label": "location",
+                "parent_id": parent,
+                "name": name
+            }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "authorization": f"Bearer {TOKEN}"
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        status = f"#{count} - Successfully Created {name}"
+        print(status)
+        return status
+    except requests.exceptions.RequestException as error:
+        status = f"#{count} - ERROR creating {name}: {error}"
+        print(status)
+        return status
+    
+def main():
+    csv_data = read_csv()
+    count = 0
+    output_file = 'output.csv'
+
+    for row in csv_data:
+        name = row['name']
+        parent = row.get('parent', None)
+        old_id = row['site_id']
+        status = create_site(name, parent, count)
+        df = pd.DataFrame({
+            'count': [count],
+            'SiteName': [name],
+            'Status': [status],
+            'old_id': [old_id]
+        })
+        df.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
+        count += 1
+
+main()
